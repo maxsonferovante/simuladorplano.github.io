1b71fbaa3634f1b51d2243013275db1cda9e8df1
maxsonferovante <maxsonferovante@gmail.com>
2021-03-22 23:32:45 -0300

    calculadora de plano ok - falta mostrar descontos

--- scripts/plano.js
+++ scripts/plano.js
@@ -68,14 +68,59 @@ $(document).ready(function(){
             0,
             0
         ];
-        var $inputSalario = document.querySelector("#inputSalario");
+        var $valorEnfer = [
+            170.95,
+            213.69,
+            256.42,
+            307.70,
+            338.47,
+            372.33,
+            409.56,
+            470.99,
+            588.73,
+            883.10
+        ];
+        var $valorApart = [
+            208.33,
+            260.41,
+            312.49,
+            374.99,
+            412.47,
+            453.74,
+            499.12,
+            573.98,
+            717.48,
+            1076.21
+        ];
+        var $valorAddApart = [
+            37.38,
+            46.72,
+            56.07,
+            67.29,
+            74.02,
+            81.41,
+            89.56,
+            102.99,
+            128.75,
+            193.11
+        ];
+
+        var $inputSalario = document.querySelector("input");
         
         var $btnCalculoPlano = document.querySelector("button");
 
-        var $selectAcomodacao = document.querySelector("#selectAcomodacao");
+        var $selectAcomodacao = document.querySelector("select");
+
+        var $aparAdd = document.querySelector("div.aparAdd");
+
+        var $resultado = document.querySelector("div.resultado");
+ 
+        var $nodeListInputBeneficiarios = Array.from(document.querySelectorAll(".numBeneciario"));
 
         console.log($inputSalario.value + " ---> " + calcuraSalario());
         
+       
+
         function calcuraSalario() {
 
             
@@ -89,15 +134,15 @@ $(document).ready(function(){
 
             $valor[1] = 0;
             $provDireitos[1] = parseFloat($inputSalario.value * ($ref_alterada[1] / 100.0));
-            console.log($provDireitos);
-
+        
+        
             $valor[2] = 0;
             $provDireitos[2] = 0;
 
 
             $valor[3] = ((parseFloat($inputSalario.value) + $provDireitos[1]) / 180) * 0.4;
             $provDireitos[3] = $valor[3] * $ref_alterada[3];
-            console.log($provDireitos);
+  
 
             $valor[4] = ((parseFloat($inputSalario.value) + $provDireitos[1]) / 180) * 1.5;
             $provDireitos[4] = $valor[4] * $ref_alterada[4];
@@ -116,10 +161,6 @@ $(document).ready(function(){
 
             $valor[7] = ((parseFloat($inputSalario.value) + $provDireitos[1] + $provDireitos[8]) / 180.0) * 2.0;
             $provDireitos[7] = $valor[7] * $ref_alterada[7];
-
-
-            console.log($provDireitos);
-
 
             $valor[9] = ((parseFloat($inputSalario.value) + $provDireitos[1]) / 180) * 2.0;
             $provDireitos[9] = $valor[9] * $ref_alterada[9];
@@ -159,17 +200,108 @@ $(document).ready(function(){
             return Math.round10(temp, -2);
 
         }
+       
+        function mostrarResultado(valorPlano){
+           /*  $resultado.append(
+                <div class="card">
+                <h5 class="card-header">Simulação do plano de saúde</h5>
+                <div class="card-body">
+                <table class="table table-bordered">
 
+                <tbody>
+                    <tr>
+                        <td>Mensalidade do Plano</td>
+                        <td>valorPlano</td>
+                    </tr>
+                    <tr>
+                        <td>Proposto pela CDP</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+                </table>
+                </div>
+                </div>              
+            ); */
+        }
         function bindEvents(){
-            $btnCalculoPlano.onclick = function(){
-                console.log("passei aqui");
-                switch ($selectAcomodacao.selectedIndex) {
+
+           $selectAcomodacao.onchange = function(){
+                switch ($selectAcomodacao.options[$selectAcomodacao.selectedIndex].value) {
+                    case "0":
+                        $aparAdd.innerHTML = "";
+                        break;
                     case "1":
-                        console.log("1");
+                        $aparAdd.innerHTML = "";
                         break;
                     case "2":
-                        console.log("2");
+                        $aparAdd.innerHTML = "";
+                        $aparAdd.innerHTML = "<label>Possiu Apartamento Adicional ?</label>" +
+                            "<div class='form-check' >"+
+                                "<input class=form-check-input type='radio' name='flexRadioDefault' id='flexRadioDefault1'>"+
+                                "<label class='form-check-label' for='flexRadioDefault1'>Sim</label>"+
+                              "</div>"+
+                              "<div class='form-check'>"+
+                                "<input class=form-check-input type='radio' name='flexRadioDefault' id='flexRadioDefault2' checked>"+
+                                "<label class='form-check-label' for='flexRadioDefault2'>Não</label>"+
+                            "</div>";
                         break;
+                    default:
+                        alert("Selecione o tipo de acomodação!");
+                }
+            }; 
+            
+            
+            $btnCalculoPlano.onclick = function(){
+
+                switch ($selectAcomodacao.options[$selectAcomodacao.selectedIndex].value) {
+                    case "1":
+                     
+
+                        var temp = 0;
+                        for (i = 0;i<$nodeListInputBeneficiarios.length;i++){
+                          
+                            temp = temp + ($nodeListInputBeneficiarios[i].value * $valorEnfer[i]);
+                        }
+                        console.log(temp);   
+                        
+
+                    break;
+                    case "2":
+                       
+                        var $nodeListRadioApartamentAdd = Array.from(document.querySelectorAll(".form-check-input"));                        
+                       
+                        if($nodeListRadioApartamentAdd[0].checked == true && $nodeListRadioApartamentAdd[1].checked == false){
+                           
+                                var temp = 0;
+                                for (i = 0;i < $nodeListInputBeneficiarios.length; i++){
+                                   
+                                    if ($nodeListInputBeneficiarios[i].value == 0){
+                                        temp = temp + ($nodeListInputBeneficiarios[i].value * $valorApart[i] );
+                                       
+                                    }
+                                    else{
+                                        temp = temp + ($nodeListInputBeneficiarios[i].value * $valorApart[i] ) + $valorAddApart[i];
+                                    }
+                                                                        
+                                }
+                                console.log(temp);
+                        }else{
+                            if($nodeListRadioApartamentAdd[0].checked == false && $nodeListRadioApartamentAdd[1].checked == true){
+                               
+                                    var temp = 0;
+                                    for (i = 0;i<$nodeListInputBeneficiarios.length;i++){
+                                      
+                                        temp = temp + ($nodeListInputBeneficiarios[i].value * $valorApart[i]);
+                                    } 
+                                    console.log(temp);   
+                            }
+                             
+                        }
+                        
+                        
+                        break;
+                    default:
+                        alert("Selecione o tipo de acomodação!");
                 }
             };
         }    
